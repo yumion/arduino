f1c98972f76b64f682cf7875461bab496fb647f5
yumion <yumion28@gmail.com>
2019-12-17 17:38:14 +0900

    8bitで1つのモータを動かすserialを送るプログラム作製

--- arduinoNano/multiServo_dcMotor_pwm/multiServo_dcMotor_pwm.ino
+++ arduinoNano/multiServo_dcMotor_pwm/multiServo_dcMotor_pwm.ino
@@ -8,17 +8,17 @@ Servo VERTICAL;  // 腕の横スライド
 #define BIN1 5  // 左輪
 #define BIN2 6
 
-int channel[5];
+int channel[6];
 
 /* sample
  * right wheel, left wheel,4本指,縦振り,横スライド
- * DEFAULT: 0,0,180,0,90e
- * MIN: 0,0,0,0,0e
- * MAX: 100,100,180,180,180e
+ * DEFAULT: 0,0,0,0,0e
+ * MIN: 0,0,0,0,-90e
+ * MAX: 100,100,100,100,90e
  */
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
 //  Serial.setTimeout(50);
   TCCR2B &= B11111000;  // 3,11番ピンの周波数を980Hzに変更
   TCCR2B |= B00000011;  // r=32の場合
@@ -35,8 +35,6 @@ void setup() {
 void dc_motor_forward(int r_speeds, int l_speeds, int period=100) {
   /* control speed by digitalWrite like PWM */
   // IN1
-  digitalWrite(AIN1, LOW);
-  digitalWrite(BIN1, LOW);
   if (r_speeds > l_speeds) {
     delay(period - r_speeds);
     digitalWrite(AIN1, HIGH); 
@@ -57,10 +55,12 @@ void dc_motor_forward(int r_speeds, int l_speeds, int period=100) {
     }
     delay(r_speeds);
   }
+  digitalWrite(AIN1, LOW);
+  digitalWrite(BIN1, LOW);
   // IN2
   digitalWrite(AIN2, LOW);
   digitalWrite(BIN2, LOW);
-//  Serial.println("forward");
+//  Serial.println(r_speeds, l_speeds);
 }
 
 
@@ -113,15 +113,16 @@ void getMonitorInput(char buffer[], uint8_t maxSize=20) {
 }
 
 
-void getInputValue(char buf[]) {
+void getInputValue(char buffer[]) {
+//  memset(buffer, 0, sizeof(buffer) / sizeof(int));
   /* serialで受け取った文字列をモータのパラメータに変換 */
   int i = 0;
   while (Serial.available()) {
-    buf[i] = Serial.read();
-    if (buf[i] == 'e') {
-      buf[i] = '\0';
-      //Serial.println(buf);
-      channel[0] = atoi(strtok(buf, ","));
+    buffer[i] = Serial.read();
+//    Serial.println(buffer);
+    if (buffer[i] == 'e') {
+      buffer[i] = '\0';
+      channel[0] = atoi(strtok(buffer, ","));
       for(int n=1; n<sizeof(channel)/sizeof(int); n++){
         channel[n] = atoi(strtok(NULL, ","));
       }
@@ -142,19 +143,25 @@ void getInputValue(char buf[]) {
 
 void loop() {
   /* get parameter from python */
-  char buf[21];
+  char buf[30];
   getInputValue(buf);
-  /* driver dc motor */
-  if (channel[0] >= 0) {
-    dc_motor_forward(channel[0], channel[1]);
-  }
-  else {
-    dc_motor_backward(channel[0], channel[1]);
-  }
-  /* drive servo */
-  GRASP.write(map(channel[2], 0, 180, 145, 180));
-  HORIZONTAL.write(map(channel[3], 0, 180, 55, 150));
-  VERTICAL.write(channel[4]);
+  
+//  if (channel[5] == 1) {
+    /* driver dc motor */
+    if (channel[0] >= 0) {
+      dc_motor_forward(channel[0], channel[1]);
+    }
+    else {
+      dc_motor_backward(channel[0], channel[1]);
+    }
+    /* drive servo */
+    GRASP.write(map(channel[2], 0, 100, 0, 50));
+    HORIZONTAL.write(map(channel[3], 0, 100, 58, 140));
+    VERTICAL.write(map(channel[4], -90, 90, 0, 180));
+//  }
+//  else {
+//    dc_motor_forward(0, 0);
+//  }
 }
 
 
---
+++ bitwise_serial.py
@@ -0,0 +1,45 @@
+# coding: utf-8
+import serial
+import time
+import sys
+
+
+ser = serial.Serial(port=sys.argv[1], baudrate=115200)
+
+
+# シリアル通信
+def send_serial(motor, value, isreading=False):
+    '''シリアル通信'''
+    send = motor * 32 + value  # 8bitを10進数で表記
+    send = send.to_bytes(1, 'big')  # byteに変換(ASCIIで表示される)
+    ser.write(send)
+    print(f"send: {send}, int: {int.from_bytes(send, 'big')}, bit: {format(int.from_bytes(send, 'big'), '08b')}")
+    # read
+    if isreading:
+        while ser.inWaiting() > 0:
+            read = ser.readline()
+            read = read.strip().decode('utf-8')
+            print(read)
+
+
+r_motor = 0
+l_motor = 0
+
+time.sleep(5)
+
+while True:
+    if r_motor < 20:
+        r_motor += 1
+        l_motor += 1
+    else:
+        r_motor = 0
+        l_motor = 0
+    params = [r_motor, l_motor]
+    for i, param in enumerate(params):
+        send_serial(i, param)
+    send_serial(5, 0, True)  # decide dc motor
+    print('-----------------')
+    time.sleep(1)
+
+
+ser.close()
